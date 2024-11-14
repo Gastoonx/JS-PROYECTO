57d6a0a3a7f02a3de927b32f520172c0883868c0
Gaston <gastinyaguston@gmail.com>
2024-11-14 19:43:37 -0300

    tercerapreentrega

--- js/main.js
+++ js/main.js
@@ -1,71 +1,103 @@
-let diasSemana = [
-    { id: 1, nombre: "Lunes" },
-    { id: 2, nombre: "Martes" },
-    { id: 3, nombre: "Miércoles" },
-    { id: 4, nombre: "Jueves" },
-    { id: 5, nombre: "Viernes" },
-    { id: 6, nombre: "Sábado" },
-    { id: 7, nombre: "Domingo" }
-];
+const carrito = document.querySelector('#carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito'); 
+let articulosCarrito = [];
 
-let turnos = [
-    { id: 1, nombre: "Mañana", horario: "6am - 14pm" },
-    { id: 2, nombre: "Tarde", horario: "14pm - 22pm" },
-    { id: 3, nombre: "Noche", horario: "22pm - 6am" }
-];
 
-// Funcion para seleccionar un día de la semana
-function seleccionarDia() {
-    let diaSeleccionado = null;
-    while (!diaSeleccionado) {
-        let numeroDia = prompt("Ingresa un número del 1 al 7 para elegir el día de la semana:");
-        
-        if (!numeroDia || isNaN(numeroDia)) {
-            alert("Por favor, ingresa un número válido.");
-            continue; // Repete el bucle si la entrada no es valida
-        }
+cargarEventListeners();
 
-        numeroDia = parseInt(numeroDia);
-        diaSeleccionado = diasSemana.find(dia => dia.id === numeroDia);
-        
-        if (!diaSeleccionado) {
-            alert("El número ingresado no corresponde a un día de la semana.");
-        }
+function cargarEventListeners() {
+   
+     listaCursos.addEventListener('click', agregarCurso);
+
+
+     carrito.addEventListener('click', eliminarCurso);
+
+
+     vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
+
+}
+
+function agregarCurso(e) {
+    e.preventDefault();
+    if(e.target.classList.contains('agregar-carrito')) {
+         const curso = e.target.parentElement.parentElement;
+         leerDatosCurso(curso);
+    }
+}
+function leerDatosCurso(curso) {
+    const infoCurso = {
+         imagen: curso.querySelector('img').src,
+         titulo: curso.querySelector('h4').textContent,
+         precio: curso.querySelector('.precio span').textContent,
+         id: curso.querySelector('a').getAttribute('data-id'), 
+         cantidad: 1
     }
 
-    console.log("El día seleccionado es: " + diaSeleccionado.nombre);
-    return diaSeleccionado;
-}
 
-// Funcion para elegir un turno
-function seleccionarTurno() {
-    let turnoSeleccionado = null;
-    while (!turnoSeleccionado) {
-        let turnoId = prompt("Elige un turno: \n1. Turno Mañana (6am - 14pm)\n2. Turno Tarde (14pm - 22pm)\n3. Turno Noche (22pm - 6am)");
-        
-        if (!turnoId || isNaN(turnoId)) {
-            alert("Por favor, ingresa un número válido.");
-            continue; // Repite el bucle si la entrada no es valida
-        }
-
-        turnoId = parseInt(turnoId);
-        turnoSeleccionado = turnos.find(turno => turno.id === turnoId);
-        
-        if (!turnoSeleccionado) {
-            alert("No seleccionaste un turno válido.");
-        }
+    if( articulosCarrito.some( curso => curso.id === infoCurso.id ) ) { 
+         const cursos = articulosCarrito.map( curso => {
+              if( curso.id === infoCurso.id ) {
+                   curso.cantidad++;
+                    return curso;
+               } else {
+                    return curso;
+            }
+         })
+         articulosCarrito = [...cursos];
+    }  else {
+         articulosCarrito = [...articulosCarrito, infoCurso];
     }
 
-    console.log("Turno seleccionado: " + turnoSeleccionado.nombre);
-    return turnoSeleccionado;
+  // console.log(articulosCarrito)
+
+  
+
+ // console.log(articulosCarrito)
+ carritoHTML();
 }
 
-// Funcion para mostrar en el html
-function mostrarResultado(dia, turno) {
-    let resultadoDiv = document.getElementById("resultado");
-    resultadoDiv.innerHTML = `Elegiste el día <strong>${dia.nombre}</strong> en el turno <strong>${turno.nombre}</strong> (${turno.horario}).`;
+function eliminarCurso(e) {
+    e.preventDefault();
+    if(e.target.classList.contains('borrar-curso') ) {
+         // e.target.parentElement.parentElement.remove();
+         const cursoId = e.target.getAttribute('data-id')
+         
+         // Eliminar del arreglo del carrito
+         articulosCarrito = articulosCarrito.filter(curso => curso.id !== cursoId);
+
+         carritoHTML();
+    }
 }
 
-let diaSeleccionado = seleccionarDia();
-let turnoSeleccionado = seleccionarTurno();
-mostrarResultado(diaSeleccionado, turnoSeleccionado);
+function carritoHTML() {
+
+    vaciarCarrito();
+
+    articulosCarrito.forEach(curso => {
+         const row = document.createElement('tr');
+         row.innerHTML = `
+              <td>  
+                   <img src="${curso.imagen}" width=100>
+              </td>
+              <td>${curso.titulo}</td>
+ <td>${curso.precio}</td>
+ <td>${curso.cantidad} </td>
+ <td>
+                <a href="#" class="borrar-curso" data-id="${curso.id}">X</a>
+   </td>
+         `;
+         contenedorCarrito.appendChild(row);
+    });
+
+}
+
+function vaciarCarrito() {
+
+
+
+    while(contenedorCarrito.firstChild) {
+contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+ }
+}
